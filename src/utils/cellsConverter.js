954c525a84b8d085917f99d4a83b5608bddcb855
Bob Dunn <rpdunn@gmail.com>
2017-06-15 21:11:37 -0600

    create trivially passing case for converting dead 1x1 population

--- src/utils/cellsConverter.js
+++ src/utils/cellsConverter.js
@@ -1,3 +1,5 @@
-const fromCellsToArray = function(){}
+const fromCellsToArray = function(){
+  return [[0]]
+}
 
 export default { fromCellsToArray }
--- src/utils/cellsConverter.test.js
+++ src/utils/cellsConverter.test.js
@@ -3,6 +3,6 @@ import cellsConverter from './cellsConverter'
 describe('#fromCellsToArray', () => {
   it('should convert empty population to dead grid', () => {
     const grid = cellsConverter.fromCellsToArray({height:1, width:1, cells:[]})
-    
+    expect(grid).toEqual([[0]])
   })
 })
