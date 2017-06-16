b8086f12509dcea75178151d9f7fc2a57c6b2240
Bob Dunn <rpdunn@gmail.com>
2017-06-15 21:24:41 -0600

    add simple implementation for 1x1 population

--- src/utils/cellsConverter.js
+++ src/utils/cellsConverter.js
@@ -1,5 +1,9 @@
-const fromCellsToArray = function(){
-  return [[0]]
+const fromCellsToArray = function(population){
+  if(population.cells.length) {
+    return [[1]]
+  } else {
+    return [[0]]
+  }
 }
 
 export default { fromCellsToArray }
--- src/utils/cellsConverter.test.js
+++ src/utils/cellsConverter.test.js
@@ -5,4 +5,8 @@ describe('#fromCellsToArray', () => {
     const grid = cellsConverter.fromCellsToArray({height:1, width:1, cells:[]})
     expect(grid).toEqual([[0]])
   })
+  it('should convert live 1x1 population to live grid', () => {
+    const grid = cellsConverter.fromCellsToArray({height:1, width:1, cells:[{x:0,y:0}]})
+    expect(grid).toEqual([[1]])
+  })
 })
